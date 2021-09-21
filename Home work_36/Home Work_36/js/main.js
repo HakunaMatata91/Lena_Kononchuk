929916789c251cd92f112d54fa3f1ca82ee25639
Lena Kononchuk <kononchuk.elena91@gmail.com>
2021-09-21 21:27:27 +0300

    add hw 36

--- Home work_36/Home Work_36/js/main.js
+++ Home work_36/Home Work_36/js/main.js
@@ -7,14 +7,17 @@ $("document").ready(function () {
     $(".close").click(function () {
         $(".wraper_modal").fadeToggle();
     });
+
+    // Пользователь нажимает кнопку Уточнить детали и открывается textarea
     $(".label").on('click', function () {
         $(".text").slideDown(500);
     });
+        // Пользователь нажимает кнопку Уточнить детали и закрыватся textarea
     $(".label").on('click', function () {
         $(".text").hide();
     });
 
-    //При наведении Желаемая дата приема меняеться на формат dd/mm/yyyy"
+    //При наведении Желаемая дата приема меняеться на формат dd/mm/yyyy
     date.onmouseover = function () {
         this.placeholder = "dd/mm/yyyy";
     }
